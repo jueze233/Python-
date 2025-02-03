42a5611e5b7f4b77e692040225200d51d525c593
Excrain <2493277759@qq.com>
2025-02-03 11:19:18 +0800

    jpg

--- pac_hong.py
+++ pac_hong.py
@@ -8,8 +8,10 @@ header = {
 response = requests.get(
     'https://p0.meituan.net/movie/ce4da3e03e655b5b88ed31b5cd7896cf62472.jpg@464w_644h_1e_1c', 
     headers=header, 
-    #proxies={'http': None, 'https': None}  # 如果开了vpn需要这行代码来禁用代理
+    proxies={'http': None, 'https': None}  # 如果开了vpn需要这行代码来禁用代理
 )
 
-print(response)
+print(response.text)
 
+with open('pac_hong.jpg', 'wb') as file:
+    file.write(response.content)
