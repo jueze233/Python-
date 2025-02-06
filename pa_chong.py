2bb50b4fe848bff341125e9374ffbb3018f228f4
Excrain <2493277759@qq.com>
2025-02-06 16:32:34 +0800

    去除with语句

--- pa_chong.py
+++ pa_chong.py
@@ -18,28 +18,27 @@ for page in range(1, 11):
     result = soup.find_all(name='div', class_='p-h el-col el-col-24 el-col-xs-9 el-col-sm-13 el-col-md-16')
 
     for i in range(len(result)):
-        with open('movie.txt', 'a', encoding='utf-8') as f:
-            moive_info['电影名字'].append(result[i].h2.string)  
+        moive_info['电影名字'].append(result[i].h2.string)  
 
-            button_list = result[i].find_all(name='button', class_='el-button category el-button--primary el-button--mini')
-            moive_type = []
-            for btn in button_list:
-                moive_type.append(btn.span.string)
-            moive_type = ','.join(moive_type)  
-            moive_info['类型'].append(moive_type)
-                
-            info_list = result[i].find_all(name='div', class_='m-v-sm info')
-            span_list = info_list[0].find_all(name='span')
-            moive_info['国家'].append(span_list[0].string)
-            moive_info['时长'].append(span_list[2].string)
-            span_list = info_list[1].find_all(name='span')
-            if len(span_list) > 0:
-                moive_info['上映时间'].append(span_list[0].string)
-            else:
-                moive_info['上映时间'].append('未知')
-                        
-            score = soup.find_all(name='p', class_='score m-t-md m-b-n-sm')
-            moive_info['电影评分'].append(score[i].string.strip() )  
+        button_list = result[i].find_all(name='button', class_='el-button category el-button--primary el-button--mini')
+        moive_type = []
+        for btn in button_list:
+            moive_type.append(btn.span.string)
+        moive_type = ','.join(moive_type)  
+        moive_info['类型'].append(moive_type)
+            
+        info_list = result[i].find_all(name='div', class_='m-v-sm info')
+        span_list = info_list[0].find_all(name='span')
+        moive_info['国家'].append(span_list[0].string)
+        moive_info['时长'].append(span_list[2].string)
+        span_list = info_list[1].find_all(name='span')
+        if len(span_list) > 0:
+            moive_info['上映时间'].append(span_list[0].string)
+        else:
+            moive_info['上映时间'].append('未知')
+                    
+        score = soup.find_all(name='p', class_='score m-t-md m-b-n-sm')
+        moive_info['电影评分'].append(score[i].string.strip() )  
             
 data = pd.DataFrame(moive_info)
 data.to_excel('./info.xlsx', index=False)
